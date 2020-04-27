afff2b89aba217b22b65f3a123d90bad5bd247a8
Awaly Mohamad <awaly.mohamad@gmail.com>
2020-04-27 20:19:04 +0200

    Creation de la class pcBureau

--- src/com/momo/pcBureau.java
+++ src/com/momo/pcBureau.java
@@ -1,5 +1,13 @@
 package com.momo;
 
 public class pcBureau {
-    System.out.print("test");
+    private String pc;
+    private String portable;
+    private int quandtite;
+
+    public pcBureau() {
+        pc = null;
+        portable = null;
+        quandtite =0;
+    }
 }
