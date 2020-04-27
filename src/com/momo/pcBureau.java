70873f35270fdb1060594530d0c5a0049d68fe53
awaly.mohamad@gmail.com <awaly.mohamad@gmail.com>
2020-04-27 20:28:06 +0200

    pcBureau : GEnere GEtters and Setters

--- src/com/momo/pcBureau.java
+++ src/com/momo/pcBureau.java
@@ -3,11 +3,35 @@ package com.momo;
 public class pcBureau {
     private String pc;
     private String portable;
-    private int quandtite;
+    private int    quandtite;
 
     public pcBureau() {
         pc = null;
         portable = null;
-        quandtite =0;
+        quandtite = 0;
+    }
+
+    public String getPc() {
+        return pc;
+    }
+
+    public String getPortable() {
+        return portable;
+    }
+
+    public int getQuandtite() {
+        return quandtite;
+    }
+
+    public void setPc( String pc ) {
+        this.pc = pc;
+    }
+
+    public void setPortable( String portable ) {
+        this.portable = portable;
+    }
+
+    public void setQuandtite( int quandtite ) {
+        this.quandtite = quandtite;
     }
 }
